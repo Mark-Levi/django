17e3ff9466109769989c1a8cf0e70b7d4c7a46bd
Mark-Levi <nessy-777@yandex.ru>
2024-03-20 16:42:09 +0300

    site2

--- nessy/mysite/views.py
+++ nessy/mysite/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
+
 # Create your views here.
+def index(request):
+    return HttpResponse("Страница моего сайта")
+
+
+def categories(request, catid):
+    if request.GET:
+        print(request.GET)
+    return HttpResponse(f"<h1>Страница категорий</h1><p>{catid}</p>")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
--- nessy/nessy/urls.py
+++ nessy/nessy/urls.py
@@ -14,9 +14,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from mysite.views import pageNotFound
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    path("", include("mysite.urls")),
 ]
+
+handler404 = pageNotFound
