c5847f743c8e283644e3c9d2973b53e0668b29ce
Mark-Levi <nessy-777@yandex.ru>
2024-03-23 15:40:56 +0300

    new site

--- coolsite/coolsite/urls.py
+++ coolsite/coolsite/urls.py
@@ -1,8 +1,7 @@
-"""
-URL configuration for nessy project.
+"""coolsite URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -14,18 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.urls import path
+
+from coolsite import settings
+from women.views import *
 from django.urls import path, include
 
-from mysite.views import pageNotFound
-from nessy import settings
-
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("mysite.urls")),
+    path('admin/', admin.site.urls),
+    path('', include('women.urls')),
 ]
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
 handler404 = pageNotFound
--- nessy/mysite/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import index, categories
-
-urlpatterns = [
-    path("", index, name="main"),
-    path("cats/<int:catid>/", categories),
-]
