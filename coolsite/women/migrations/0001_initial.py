768ec670738773525c6b6caaa26e3337ae159170
Mark-Levi <nessy-777@yandex.ru>
2024-03-24 18:04:52 +0300

    2 base

--- coolsite/women/migrations/0001_initial.py
+++ coolsite/women/migrations/0001_initial.py
@@ -7,20 +7,27 @@ class Migration(migrations.Migration):
 
     initial = True
 
-    dependencies = [
-    ]
+    dependencies = []
 
     operations = [
         migrations.CreateModel(
-            name='Women',
+            name="Women",
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('title', models.CharField(max_length=255)),
-                ('content', models.TextField(blank=True)),
-                ('photo', models.ImageField(upload_to='photos/%Y/%m/%d/')),
-                ('time_create', models.DateTimeField(auto_now_add=True)),
-                ('time_update', models.DateTimeField(auto_now=True)),
-                ('is_published', models.BooleanField(default=True)),
+                (
+                    "id",
+                    models.AutoField(
+                        auto_created=True,
+                        primary_key=True,
+                        serialize=False,
+                        verbose_name="ID",
+                    ),
+                ),
+                ("title", models.CharField(max_length=255)),
+                ("content", models.TextField(blank=True)),
+                ("photo", models.ImageField(upload_to="photos/%Y/%m/%d/")),
+                ("time_create", models.DateTimeField(auto_now_add=True)),
+                ("time_update", models.DateTimeField(auto_now=True)),
+                ("is_published", models.BooleanField(default=True)),
             ],
         ),
     ]
